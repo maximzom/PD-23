5df270b15559d88c3eb2d50dc56dbe67cad5f763
maximzom <86885085+maximzom@users.noreply.github.com>
2024-03-15 15:09:48 +0200

    Add files via upload

---
+++ CSharpLabs2024/Pr3/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pr3_Points_And_Figure
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+        public string Name { get; }
+
+        public Point(int x, int y, string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+
+    }
+}
